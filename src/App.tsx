87fcccc0504a6e7154fc768bcd1e02a4374f5a85
Vinicius Duarte <146869828+ViniCellist@users.noreply.github.com>
2024-06-17 19:08:33 -0300

    feat axios get and post

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './paginas/Home';
 import VitrineRestaurantes from './paginas/VitrineRestaurantes';
+import AdministracaoRestaurante from './paginas/Administracao/Restaurante/AdministracaoRestaurante';
+import FormularioRestaurante from './paginas/Administracao/Restaurante/FormularioRestaurante';
 
 function App() {
 
@@ -8,6 +10,8 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/restaurantes" element={<VitrineRestaurantes />} />
+      <Route path="/admin/restaurantes" element={<AdministracaoRestaurante />} />
+      <Route path="/admin/restaurantes/novo" element={<FormularioRestaurante />} />
     </Routes>
   );
 }
